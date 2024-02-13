bb2411b850635b9219b6ce1f2dc5758446171f2b
iamoros <ivan13ap@gmail.com>
2024-02-13 11:31:38 +0100

    first commit

---
+++ src/MainPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function MainPage() {
+  const handleLinkedInClick = () => {
+    window.open('https://www.linkedin.com/in/ivan-amoros-prados/', '_blank');
+  };
+
+  React.useEffect(() => {
+    document.title = "Ivan Amorós";
+  }, []);
+
+  return (
+    <div>
+      <h1>UNDER CONSTRUCTION</h1>
+      <p onClick={handleLinkedInClick} style={{ cursor: 'pointer' }}>
+        <img src="/images/LinkedIn_icon.png" alt="LinkedIn Icon" style={{ marginRight: '5px', width: '20px', height: '20px' }} />
+        <span className="underline-on-hover">Ivan Amorós Prados</span>
+      </p>
+    </div>
+  );
+}
+
+export default MainPage;
