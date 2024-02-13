59d4395c26b416e23752c4ce1a908071fbb2913d
iamoros <ivan13ap@gmail.com>
2024-02-13 18:09:26 +0100

    base

--- src/CookieConsentComponent.js
+++ src/CookieConsentComponent.js
@@ -4,7 +4,7 @@ import CookieConsent from 'react-cookie-consent';
 const CookieConsentComponent = ({ onAccept }) => {
   return (
     <CookieConsent onAccept={onAccept}>
-      🍪 This website uses cookies to ensure you get the best experience on our website.
+      🍪 This website uses cookies to ensure you get the best experience on the website.
       {/* <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span> */}
     </CookieConsent>
   );
---
+++ src/ExamplePage.js
@@ -0,0 +1,14 @@
+// src/ExamplePage.js
+
+import React from 'react';
+
+function ExamplePage() {
+  return (
+    <div>
+      <h1>Example Page</h1>
+      <p>This is an example page.</p>
+    </div>
+  );
+}
+
+export default ExamplePage;
