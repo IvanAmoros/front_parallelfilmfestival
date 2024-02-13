b2b9e954032fbcf766408743b4944e266b9b1659
iamoros <ivan13ap@gmail.com>
2024-02-13 17:10:21 +0100

    cookies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import MainPage from './MainPage'; // Importing MainPage component
+import MainPage from './MainPage';
+import CookieConsentComponent from './CookieConsentComponent'; // Make sure the import path is correct
 
 function App() {
+  const [analyticsInitialized, setAnalyticsInitialized] = useState(false);
+
+  const initializeAnalytics = () => {
+    if (!analyticsInitialized) {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      window.gtag = function() { window.dataLayer.push(arguments); };
+      window.gtag('js', new Date());
+      window.gtag('config', 'G-NF487Q1T0Z');
+      setAnalyticsInitialized(true);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <MainPage /> {/* Rendering MainPage component */}
+        <MainPage />
+        <CookieConsentComponent onAccept={initializeAnalytics} />
       </header>
     </div>
   );
