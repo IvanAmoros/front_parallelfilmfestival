dc8a3369001c6f58535fd2b9b55d7524e9ff2136
iamoros <ivan13ap@gmail.com>
2024-02-21 18:03:22 +0100

    skills display

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 function MainPage() {
-  const handleLinkedInClick = () => {
-    window.open('https://www.linkedin.com/in/ivan-amoros-prados/', '_blank');
-  };
 
   React.useEffect(() => {
     document.title = "Ivan Amorós";
@@ -18,10 +15,6 @@ function MainPage() {
       ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
       fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
       mollit anim id est laborum.
-      </p>
-      <p onClick={handleLinkedInClick} style={{ cursor: 'pointer' }}>
-				<img src={`${process.env.PUBLIC_URL}/images/LinkedIn_icon.png`} alt="LinkedIn Icon" style={{ marginRight: '5px', width: '20px', height: '20px' }}/>
-        <span className="underline-on-hover">Ivan Amorós Prados</span>
       </p>
     </div>
   );
